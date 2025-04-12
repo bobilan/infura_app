2c23ecba313926d93cb70a776403e21a7627251d
bobilan <bohdanych@gmail.com>
2025-04-12 23:30:35 +0200

    Add streaming client

--- tests/clients/test_infura.py
+++ tests/clients/test_infura.py
@@ -1,4 +1,3 @@
-
 import requests
 import pytest
 from pytest_httpserver import HTTPServer, RequestMatcher
@@ -7,33 +6,6 @@ from clients.infura import InfuraClient, InfuraApiPayload
 
 
 class TestInfuraClient:
-    # def test_get_balance_returns_expected_result(self, httpserver: HTTPServer) -> None:
-    #     # given
-    #     infura_client = InfuraClient(base_url=httpserver.url_for("get-wallet"))
-    #     payload = InfuraApiPayload(
-    #         jsonrpc="test",
-    #         method="get_walletBalance",
-    #         params=["wallet_id", "1"],
-    #         id=1
-    #     )
-    #     expected_response = {
-    #             "jsonrpc": "0.1",
-    #             "id": 1,
-    #             "result": "test",
-    #     }
-    #     httpserver.expect_request("/get-wallet", method="POST").respond_with_json(
-    #         expected_response
-    #     )
-    #
-    #     # when
-    #     client_response = infura_client.get_balance(payload=payload)
-    #
-    #     # then
-    #     assert client_response.id == expected_response["id"]
-    #     assert client_response.result == expected_response["result"]
-    #     assert client_response.jsonrpc == expected_response["jsonrpc"]
-    #
-    #     httpserver.assert_request_made(matcher=RequestMatcher(uri="/get-wallet", method="POST", json=payload.model_dump()))
 
     def test_get_balance_returns_expected_result(self, httpserver: HTTPServer) -> None:
         # given
@@ -87,7 +59,7 @@ class TestInfuraClient:
         infura_client = InfuraClient(base_url="/")
         hex_balance = "0x2540BE400"
         # when
-        result = infura_client.hexadecimal_wei_to_gwei(hex_balance)
+        result = infura_client.convert_to_gwei(hex_balance)
         # then
         assert result == 10.0
 
@@ -96,7 +68,7 @@ class TestInfuraClient:
         infura_client = InfuraClient(base_url="/")
         hex_balance = "0x0"
         # when
-        result = infura_client.hexadecimal_wei_to_gwei(hex_balance)
+        result = infura_client.convert_to_gwei(hex_balance)
         # then
         assert result == 0.0
 
@@ -156,7 +128,3 @@ class TestInfuraClient:
                 )
             )
             # TODO: how to handle unsuccessful codes
-
-
-
-# TODO: test self._hex_wei_to_eth(validated_response.result) separately
